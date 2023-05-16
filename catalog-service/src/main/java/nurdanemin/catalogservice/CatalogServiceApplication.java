a63b1a3b6059b149b7dbcc72654c97c46313a99a
Nurdan Emin <nurdanemin0@gmail.com>
2023-05-17 00:47:24 +0300

    discovery server
    discovery server

--- catalog-service/src/main/java/nurdanemin/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/nurdanemin/catalogservice/CatalogServiceApplication.java
@@ -1,9 +1,11 @@
 package nurdanemin.catalogservice;
 
+import nurdanemin.commonpackage.utils.constants.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        Paths.ConfigurationBasePackage, Paths.Catalog.ServiceBasePackage})
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
