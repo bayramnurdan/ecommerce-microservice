da7cccc7581e05cf66ccba0419e3de99373f3ccf
Nurdan Emin <nurdanemin0@gmail.com>
2023-05-17 01:04:55 +0300

    api-getway
    api-getway

---
+++ api-getway/src/main/java/nurdanemin/apigetway/ApiGetwayApplication.java
@@ -0,0 +1,13 @@
+package nurdanemin.apigetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGetwayApplication.class, args);
+    }
+
+}
---
+++ api-getway/src/test/java/nurdanemin/apigetway/ApiGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package nurdanemin.apigetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
