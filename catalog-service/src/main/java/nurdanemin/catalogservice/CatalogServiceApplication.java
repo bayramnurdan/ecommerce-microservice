091a8d56601cb320466de6c4c9471cfc6b6550a9
Nurdan Emin <nurdanemin0@gmail.com>
2023-05-17 19:29:17 +0300

    17/5/23
    17/5/23

--- catalog-service/src/main/java/nurdanemin/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/nurdanemin/catalogservice/CatalogServiceApplication.java
@@ -3,7 +3,9 @@ package nurdanemin.catalogservice;
 import nurdanemin.commonpackage.utils.constants.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {
         Paths.ConfigurationBasePackage, Paths.Catalog.ServiceBasePackage})
 public class CatalogServiceApplication {
